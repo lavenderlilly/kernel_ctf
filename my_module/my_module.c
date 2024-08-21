fa52174d591a17e5329040613a4aeb7f37a6348c
lavenderlilly <102565174+lavenderlilly@users.noreply.github.com>
2024-08-21 15:57:26 +0900

    Update my_module.c

--- my_module/my_module.c
+++ my_module/my_module.c
@@ -6,7 +6,7 @@
 
 MODULE_LICENSE("GPL");
 
-static ssize_t mymodule_write(struct file *file, const char __user *buf, size_t count, loff_t *p)
+static ssize_t my_write(struct file *file, const char __user *buf, size_t count, loff_t *p)
 {
     char kern_buf[64] = { 0, };
 
@@ -17,24 +17,24 @@ static ssize_t mymodule_write(struct file *file, const char __user *buf, size_t 
 }
 
 static struct proc_ops proc_file_ops = {
-    .write = mymodule_write,
+    .proc_write = my_write,
 };
 
-static int __init mymodule_init(void)
+static int __init my_init(void)
 {
     printk(KERN_ALERT "my module init!\n");
-    if (!proc_create("my module", 0666, NULL, &proc_file_ops)) {
+    if (!proc_create("my", 0666, NULL, &proc_file_ops)) {
         printk(KERN_ALERT "Failed to create proc entry\n");
         return -ENOMEM;
     }
     return 0;
 }
 
-static void __exit mymodule_exit(void)
+static void __exit my_exit(void)
 {
     remove_proc_entry("my module exit!", NULL);
     printk(KERN_ALERT "driver exit!\n");
 }
 
-module_init(mymodule_init);
-module_exit(mymodule_exit);
+module_init(my_init);
+module_exit(my_exit);
